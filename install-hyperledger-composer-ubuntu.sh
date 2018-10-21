9b17df85e210fb0b63239a66d57171cc2fdf6757
Keyur Patel <simkeyur@gmail.com>
2018-10-20 23:52:29 -0500

    Update install-hyperledger-composer-ubuntu.sh

--- install-hyperledger-composer-ubuntu.sh
+++ install-hyperledger-composer-ubuntu.sh
@@ -35,12 +35,6 @@ mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
 curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
 tar -xvf fabric-dev-servers.tar.gz
 
-#Install Hyperledger Fabric
-mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
-
-curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
-tar -xvf fabric-dev-servers.tar.gz
-
 cd ~/fabric-dev-servers
 export FABRIC_VERSION=hlfv12
 ./downloadFabric.sh
